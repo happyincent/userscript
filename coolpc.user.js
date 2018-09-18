69e035b8f518146e528eb1c8b3fe88aff3442e0f
Vincent Chen <happyincent@gmail.com>
2018-09-18 14:15:59 +0800

    Update coolpc.user.js

--- coolpc.user.js
+++ coolpc.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://www.coolpc.com.tw/evaluate.php
+// @match        *://www.coolpc.com.tw/evaluate.php
 // @grant        none
 // ==/UserScript==
 
