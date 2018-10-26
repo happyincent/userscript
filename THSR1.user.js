b3c7308c1c0e9c3e56871fcb8563af1ab372737e
Vincent Chen <happyincent@gmail.com>
2018-10-26 23:16:41 +0800

    Update THSR1.user.js

--- THSR1.user.js
+++ THSR1.user.js
@@ -8,20 +8,21 @@
 // @grant        none
 // ==/UserScript==
 
-/* 台北1 彰化7 台南10 高雄11 */
-document.querySelector("select.text1:nth-child(1)").value = 1;
-document.querySelector("select.text1:nth-child(2)").value = 10;
+/* 台北2 彰化8 台南11 高雄12 */
+document.querySelector("select.text1:nth-child(1)").value = 11;
+document.querySelector("select.text1:nth-child(2)").value = 2;
 
 document.querySelector("#seatRadio1").click();
-document.querySelector("#bookingMethod2").click();
+document.querySelector("#bookingMethod_1").click();
 
 document.querySelector("#toTimeInputField").value = "2018/04/08";
 document.querySelector("#toTrainID > input:nth-child(1)").value = 1257;
 
- /* 來回票
+/* 來回票 *
+// document.querySelector("#backDate").style.display = '';
+// document.querySelector("#backTrainID").style.display = '';
 document.querySelector("#returnCheckBox").checked = true;
 document.querySelector("#backTimeInputField").value = "2017/09/13";
-document.querySelector("#backTrainID > input:nth-child(1)").value = 696; */
+document.querySelector("#backTrainID > input:nth-child(1)").value = 696; //*/
 
 document.querySelector("span.PR10:nth-child(3) > input:nth-child(1)").focus();
-
